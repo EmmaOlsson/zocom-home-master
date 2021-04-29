89ce5dcaacda210276d53183227fb3bb7a14de3f
Emma Olsson <emma.olsson22@learnet.se>
2021-04-29 19:34:11 +0200

    updated

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { app } = require('./core');
-const { db, sse, update } = require('./db');
-
-let devices = db.get('devices');
+const { db, update } = require('./db');
 
 
-/* app.get('/devices/:type/:id/:on', (req, res) => {
-    let device = devices.find(device => device.id === req.params.id)
+let devices = db.get('devices')
+
+// AIR CONDITION
+// IDs: AC1
+app.get('/AC/:id/:on', (req, res) => {
+    let device = devices.filter(device => device.id === req.params.id)
     if (req.params.on !== 'on') {
         device.assign(
             {
@@ -27,26 +29,165 @@ let devices = db.get('devices');
             }).value()
             update()
         if(JSON.stringify(req.query) == '{}'){
-            console.log('req.query', req.query)
-            res.send(`${device.name} is ${device.state}`)
+            res.send(`${device.temperature} hej`)
         } else {
             device.assign(
                 {
-                    temperature : req.query.temperature,
-                    // brightness : req.query.brightness,
-                    // color: req.query.color
+                    temperature : req.query.temperature
+                }).value()
+                update()
+            res.send(`Temperature is ${device.temperature}`)
+        } 
+    } else {
+        res.send('Please enter a valid function - on or off')
+    }
+})
+
+// BLINDS
+// IDs: BLI1
+// Up / Down
+app.get('/blinds/:id/:on', (req, res) => {
+    let device = devices.filter(device => device.id === req.params.id)
+    if (req.params.on !== 'on') {
+        device.assign(
+            {
+                on: false,
+                state: 'up',
+            }).value()
+        update()
+        if(req.params.on === 'off'){
+            res.send(`${device.name} is up`)
+        } else {
+            res.send(`Please enter a valid function for ${device.name}`)
+        }
+    } else if (req.params.on === 'on') {
+        device.assign(
+            {
+                on : true,
+                state : 'down',
+            }).value()
+            update()
+/* ************* */
+            console.log('undefined?', typeof(device))
+            res.send(`${device.name} is down`)
+    } else {
+        res.send('Please enter a valid function - on or off')
+    }
+})
+
+// LIGHTS
+// IDs: LIG1, LIG2, LIG3
+app.get('/lights/:id/:on', (req, res) => {
+    let device = devices.filter(device => device.id === req.params.id)
+    if (req.params.on !== 'on') {
+        device.assign(
+            {
+                on: false,
+                state: 'off',
+            }).value()
+        update()
+        if(req.params.on === 'off'){
+            res.send(`${device.name} är av`)
+        } else {
+            res.send(`Please enter a valid function for ${device.name}, on or off`)
+        }
+    }
+    else if (req.params.on === 'on') {
+        device.assign(
+            {
+                on : true,
+                state : 'on',
+            }).value()
+            update()
+        if(JSON.stringify(req.query) == '{}'){
+            console.log('req.query', req.query)
+            res.send(`${device.name} is ${device.state}`)
+        } else if (req.query !== '{}'){
+            device.assign(
+                {
+                    brightness : req.query.brightness
                 }).value()
                 update()
             res.send('req queeery')
-        }
+        } 
     } else {
-        res.send('Please enter a valid function')
+        res.send('Please enter a valid function - on or off')
     }
+})
 
-}) */
+// Vacuum
+// IDs: VAC1
+app.get('/vacuum/:id/:state', (req, res) => {
+    let device = devices.filter(device => device.id === req.params.id)
+    if (req.params.on !== 'on' || 'charge') {
+        device.assign(
+            {
+                on: false,
+                state: 'off',
+            }).value()
+        update()
+        if(req.params.state === 'off'){
+            res.send(`${device.name} är av`)
+        } else {
+            res.send(`Please enter a valid function for ${device.name}`)
+        }
+    }
+    else if (req.params.state === 'on') {
+        device.assign(
+            {
+                on : true,
+                state : 'cleaning',
+            }).value()
+            update()
+        res.send(`${device.name} is ${device.state}/cleaning`)
+    } else if (req.params.state === 'charging') {
+        device.assign(
+            {
+                on : true,
+                state : 'charging',
+            }).value()
+            update()
+        res.send(`${device.name} is /charging`)
+    } else {
+        res.send('Please enter a valid function - on or off')
+    }
+})
 
-app.get('/lights/:id/:on', (req, res) => {
-    let device = devices.find(device => device.id === req.params.id)
+// Lock
+// IDs: LOC1
+app.get('/lock/:id/:on', (req, res) => {
+    let device = devices.filter(device => device.id === req.params.id)
+    if (req.params.on !== 'on') {
+        device.assign(
+            {
+                locked: false,
+                state: 'locked',
+            }).value()
+        update()
+        if(req.params.on === 'off'){
+            res.send(`${device.name} is locked`)
+        } else {
+            res.send(`Please enter a valid function for ${device.name}`)
+        }
+    }
+    else if (req.params.on === 'on') {
+        device.assign(
+            {
+                locked: true,
+                state : 'open',
+                secret: '19d493b3-9a2e-495e-9dc2-44b7836dae9a'
+            }).value()
+            update()
+        res.send(`${device.name} is open`)
+    } else {
+        res.send('Please enter a valid function - on or off')
+    }
+})
+
+// Camera
+// IDs: CAM1
+app.get('/camera/:id/:on', (req, res) => {
+    let device = devices.filter(device => device.id === req.params.id)
     if (req.params.on !== 'on') {
         device.assign(
             {
@@ -64,7 +205,38 @@ app.get('/lights/:id/:on', (req, res) => {
         device.assign(
             {
                 on : true,
-                state : 'on',
+                state : 'filming',
+                secret : 'a5e337f5-a391-4fda-894e-d14aba719c9e'
+            }).value()
+            update()
+        res.send(`${device.name} is filming`)
+    } else {
+        res.send('Please enter a valid function - on or off')
+    }
+})
+
+// Speaker
+// IDs: SPE1
+app.get('/speaker/:id/:on', (req, res) => {
+    let device = devices.filter(device => device.id === req.params.id)
+    if (req.params.on !== 'on') {
+        device.assign(
+            {
+                on: false,
+                state: 'off',
+            }).value()
+        update()
+        if(req.params.on === 'off'){
+            res.send(`${device.name} är av`)
+        } else {
+            res.send(`Please enter a valid function for ${device.name}`)
+        }
+    }
+    else if (req.params.on === 'on') {
+        device.assign(
+            {
+                on : true,
+                state : 'playing music',
             }).value()
             update()
         if(JSON.stringify(req.query) == '{}'){
@@ -81,47 +253,7 @@ app.get('/lights/:id/:on', (req, res) => {
     } else {
         res.send('Please enter a valid function - on or off')
     }
-
 })
-
-/* app.use('/', (res, req, next) => {
-    if (req.body){
-
-        device.assign(
-                {   on: true,
-                    temperature: q.temperature,
-                    // color: q.color
-                    //brightness: q.brightness * 1,
-                }).value()
-    
-    
-            update();
-            res.send(`Device: ${device.type} with ID: ${device.id} \n Location: ${device.name} \n is ${device.on}`)
-        }
-}) */
-
-
-
-/* app.get('/devices', (req, res) => {
-    console.log(req.query)
-    let q = req.query;
-    let device = devices.find(device => device.id === q.id) 
-    .assign(
-            {
-                on: q.on,
-                temperature: q.temperature,
-                state: q.state,
-                locked: q.locked
-                // color: q.color
-                //brightness: q.brightness * 1,
-            }).value()
-
-
-        update();
-        res.send(`Device: ${device.type} with ID: ${device.id} \n Location: ${device.name} \n is ${device.on}`)
-}) */
-
-
 
 app.listen(3000, () => {
     console.log('API for smart home 1.1 up n running.')
